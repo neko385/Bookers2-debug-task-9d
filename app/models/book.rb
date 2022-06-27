1b18a70d3904badf28f62a53230f4e6f8cfd8b2c
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-27 13:11:51 +0000

    [Add]モデルのアソシエーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   #favorited_by？メソッドで引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べる。
   #存在していればtrue、存在していなければfalseを返すようにする
   def favorited_by?(user)
